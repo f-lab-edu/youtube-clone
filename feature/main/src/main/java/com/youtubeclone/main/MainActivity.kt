38409147557a722566430dccfc8ec8f9e434ecbe
jungha.so <pansso@naver.com>
2024-03-25 12:12:54 +0900

    #4 MainScreen, Bottom Navigation 추가

--- feature/main/src/main/java/com/youtubeclone/main/MainActivity.kt
+++ feature/main/src/main/java/com/youtubeclone/main/MainActivity.kt
@@ -7,9 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,8 +21,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initView()
+    }
+
+    private fun initView(){
+
         setContent {
-            Text(text = "screen test")
+            val navigator = rememberNavController()
+            MainScreen(navigator)
         }
     }
 
