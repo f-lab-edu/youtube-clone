23db9ddf384c1d32471871782f461ca2d14361b8
jungha.so <pansso@naver.com>
2024-03-26 20:08:19 +0900

    #2 Response Model 이름 변경, 공통 Model추가, Mapper class 추가

--- core/data/src/main/java/com/youtubeclone/data/repository/YoutubeRepositoryImpl.kt
+++ core/data/src/main/java/com/youtubeclone/data/repository/YoutubeRepositoryImpl.kt
@@ -2,12 +2,14 @@ package com.youtubeclone.data.repository
 
 import com.youtubeclone.data.api.YoutubeApi
 import com.youtubeclone.data.model.YoutubeDataResponse
+import com.youtubeclone.data.model.mapper.toData
+import com.youtubeclone.model.YoutubeData
 import javax.inject.Inject
 
 internal class YoutubeRepositoryImpl @Inject constructor(
     private val youtubeApi: YoutubeApi,
 ) : YoutubeRepository{
-    override suspend fun getVideos(q: String, videoDuration: String): YoutubeDataResponse {
-        return youtubeApi.getVideos(q = q, videoDuration = videoDuration)
+    override suspend fun getVideos(q: String, videoDuration: String): YoutubeData {
+        return youtubeApi.getVideos(q = q, videoDuration = videoDuration).toData()
     }
 }
