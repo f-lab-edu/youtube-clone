970c205471e7a5a7b445168dcab6889dbd833baa
jungha.so <pansso@naver.com>
2024-03-27 15:22:48 +0900

    #5 code review 반영 - Enum에 사용되는 인스턴스 제거, route getter 추가

--- feature/main/src/main/java/com/youtubeclone/main/mainNavigation/MainScreenRoute.kt
+++ feature/main/src/main/java/com/youtubeclone/main/mainNavigation/MainScreenRoute.kt
@@ -2,16 +2,17 @@ package com.youtubeclone.main.mainNavigation
 
 import androidx.annotation.StringRes
 import com.youtubeclone.main.R
+import java.util.Locale
 
 /**
  * 화면 전환이 추가될때 루트 추가
  */
 
-internal enum class MainScreenRoute(
-    val route: String,
-) {
-    HOME("home"),
-    SHORTS("shorts"),
-    LIBRARY("library"),
-    SUBSCRIPTIONS("subscriptions")
+internal enum class MainScreenRoute {
+    HOME,
+    SHORTS,
+    LIBRARY,
+    SUBSCRIPTIONS;
+    val route: String
+        get() = this.name.lowercase(Locale.ROOT)
 }
